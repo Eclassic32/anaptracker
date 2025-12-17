cb6de3962d9762de034042a1b0167496a45a36c1
Thomas GUERY <originaltompouce@gmail.com>
2025-12-17 17:53:02 +0100

    Add Pokemon Fire Red and Leaf Green

---
+++ src/components/gamedata/GDataPokemonFRLG.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="">
+        <div class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 my-1 bg-opacity-25">
+            <img src="/img/pokemon_frlg/badges/boulder_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Boulder Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/cascade_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Cascade Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/thunder_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Thunder Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/rainbow_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Rainbow Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/soul_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Soul Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/marsh_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Marsh Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/volcano_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Volcano Badge')  }" />
+            <img src="/img/pokemon_frlg/badges/earth_badge.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Earth Badge')  }" />
+        </div>
+
+        <div class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 my-1 bg-opacity-25">
+            <img src="/img/pokemon_frlg/hms/hm1.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM01 Cut')  }" />
+            <img src="/img/pokemon_frlg/hms/hm2.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM02 Fly')  }" />
+            <img src="/img/pokemon_frlg/hms/hm3.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM03 Surf')  }" />
+            <img src="/img/pokemon_frlg/hms/hm4.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM04 Strength')  }" />
+            <img src="/img/pokemon_frlg/hms/hm5.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM05 Flash')  }" />
+            <img src="/img/pokemon_frlg/hms/hm6.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM06 Rock Smash')  }" />
+            <img src="/img/pokemon_frlg/hms/hm7.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('HM07 Waterfall')  }" />
+        </div>
+
+        <div class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 my-1 bg-opacity-25">
+            <img src="/img/pokemon_frlg/other/bike.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Bike')  }" />
+            <img src="/img/pokemon_frlg/other/card_key.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Card Key')  }" />
+            <img src="/img/pokemon_crystal/items/tea.png" class="inline-block mr-1 w-[16px] h-[16px]" :class="{ 'opacity-25': !getNumberItemsFromName('Tea')  }" />
+        </div>
+    </div>
+</template>
+    
+<script>
+
+export default {
+  name: "gDataPokemonFRLG",
+        props: {
+            data: Object,
+            gamedata: Object,
+            index: Number,
+            checks_done: Number,
+            total_checks: Number,
+            player_name: String,
+            player_game: String
+  },
+  data: function () {
+    return {
+    };
+  },
+
+        methods: {
+            getNumberItemsFromName: function (name) {
+                var res = 0;
+                if (this.data && this.data.player_items_received && this.gamedata && this.gamedata.location_name_to_id) {
+
+                    var id = this.gamedata.item_name_to_id[name];
+                    //console.log(this.gamedata.item_name_to_id);
+                    //console.log(this.data.player_items_received);
+                    for (var x = 0; x < this.data.player_items_received[this.index].items.length; x++) {
+                        //console.log(this.data.player_items_received[this.index].items[x])
+                        if (this.data.player_items_received[this.index].items[x][0] == id)
+                            res++;
+                    }
+                }
+                return res;
+            }
+        },
+  components: {
+  },
+};
+</script>
