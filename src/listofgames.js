e6586ca81e220aa11752969d484f91b4730fb939
Thomas GUERY <originaltompouce@gmail.com>
2025-12-18 13:37:17 +0100

    Add Factorio, Pokemon Emerald, and Pokemon FRLG to the Gamelist

--- src/listofgames.js
+++ src/listofgames.js
@@ -1,7 +1,10 @@
 
 import GDataCeleste from './components/gamedata/GDataCeleste.vue';
+import GDataFactorio from './components/gamedata/GDataFactorio.vue';
 import GDataHollowKnight from './components/gamedata/GDataHollowKnight.vue';
 import GDataPokemonCrystal from './components/gamedata/GDataPokemonCrystal.vue';
+import GDataPokemonEmerald from './components/gamedata/GDataPokemonEmerald.vue';
+import GDataPokemonFRLG from './components/gamedata/GDataPokemonFRLG.vue';
 import GDataMario64 from './components/gamedata/GDataMario64.vue';
 import GDataShipOfHarkinian from './components/gamedata/GDataShipOfHarkinian.vue';
 import GDataFFMysticQuest from './components/gamedata/GDataFFMysticQuest.vue';
@@ -18,6 +21,10 @@ var LIST_OF_GAMES = [
         'class': GDataCrystalProject
     },
     {
+        'name': 'Factorio',
+        'class': GDataFactorio
+    },
+    {
         'name': 'Final Fantasy Mystic Quest',
         'class': GDataFFMysticQuest
     },
@@ -32,6 +39,14 @@ var LIST_OF_GAMES = [
     {
         'name': 'Pokemon Crystal',
         'class': GDataPokemonCrystal
+    },
+    {
+        'name': 'Pokemon Emerald',
+        'class': GDataPokemonEmerald
+    },
+    {
+        'name': 'Pokemon FireRed and LeafGreen',
+        'class': GDataPokemonFRLG
     },
     {
         'name': 'Ship of Harkinian',
