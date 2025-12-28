e7cc9276f9ed3bbb62ddfc698e7563a55995c5e3
Thomas GUERY <originaltompouce@gmail.com>
2025-12-28 23:32:17 +0100

    Added a lot of stuff for Celeste (have to replace most part with pictures)

--- src/components/gamedata/GDataCeleste.vue
+++ src/components/gamedata/GDataCeleste.vue
@@ -2,8 +2,36 @@
     <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
         <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Goal</div>
 
-        <span class="mr-2 text-xs" :class="{ 'opacity-25': !getNumberItemsFromName('Strawberry')  }"><img src="/img/celeste/items/berry.png" />x{{ getNumberItemsFromName('Strawberry') }} </span>
+        <span v-if="getGoalStrawberries()" class="mr-2 text-xs"><span class="font-bold" :class="{ 'opacity-25': !getNumberItemsFromName('Strawberry')  }"><img src="/img/celeste/items/berry.png" />x{{ getNumberItemsFromName('Strawberry') }} </span> / {{ getGoalStrawberries() }}</span>
+        <span v-else class="mr-2 text-xs font-bold" :class="{ 'opacity-25': !getNumberItemsFromName('Strawberry')  }"><img src="/img/celeste/items/berry.png" />x{{ getNumberItemsFromName('Strawberry') }} </span>
 
+    </div>
+    <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
+        <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Abilities</div>
+
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Dash Switches')  }">DSwiches</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Dash Refills')  }">DRefills</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Double Dash Refills')  }">DDRefills</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Springs')  }">Springs</span>
+        <img src="/img/celeste/misc/blue_booster.png" :class="{ 'opacity-25': !getNumberItemsFromName('Blue Boosters')  }" />
+        <img src="/img/celeste/misc/red_booster.png" :class="{ 'opacity-25': !getNumberItemsFromName('Red Boosters')  }" />
+        <img src="/img/celeste/misc/badeline_booster.png" :class="{ 'opacity-25': !getNumberItemsFromName('Badeline Boosters')  }" />
+        <img src="/img/celeste/misc/bumper.png" :class="{ 'opacity-25': !getNumberItemsFromName('Bumpers')  }" />
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Coins')  }">Coins</span>
+        <img src="/img/celeste/misc/feather.png" :class="{ 'opacity-25': !getNumberItemsFromName('Feathers')  }" />
+        <img src="/img/celeste/misc/seeker.png" :class="{ 'opacity-25': !getNumberItemsFromName('Seekers')  }" />
+        <span class="mr-2"></span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Blue Cassette Blocks')  }">BlueCB</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Green Cassette Blocks')  }">GreenCB</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Pink Cassette Blocks')  }">PinkCB</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Yellow Cassette Blocks')  }">YellowCB</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Pink Clouds')  }">PinkClds</span>
+        <span class="mr-2"></span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Blue Blocks')  }">BlueBlks</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Move Blocks')  }">MoveBlks</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Traffic Blocks')  }">TrafficBlks</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Swap Blocks')  }">SwapBlks</span>
+        <span class="font-medium text-[9px] mr-2" :class="{ 'opacity-25': !getNumberItemsFromName('Dream Blocks')  }">DreamBlks</span>
     </div>
 </template>
     
@@ -31,6 +59,12 @@ export default {
                     return 'iconbar-L my-1';
                 }
                 return 'iconbar-S';
+            },
+            getGoalStrawberries: function () {
+                if (this.data.slot_data.strawberries_required) {
+                    return this.data.slot_data.strawberries_required;
+                }
+                return 0;
             },
             getNumberItemsFromName: function (name) {
                 var res = 0;
