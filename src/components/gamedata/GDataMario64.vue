2f2a5591fcccf24d6a14ef1d6018023fa0533418
Thomas GUERY <originaltompouce@gmail.com>
2025-12-29 19:54:06 +0100

    fix : Mario 64 shuffle only hide mario moves.

--- src/components/gamedata/GDataMario64.vue
+++ src/components/gamedata/GDataMario64.vue
@@ -19,23 +19,23 @@
     -->
         </div>
 
-        <div v-if="moveShuffled()" :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
-            <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Moves</div>
+        <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
+            <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Abilities</div>
 
             <img src="/img/sm64/blocks/block_red.png" :class="{ 'opacity-25': !getNumberItemsFromName('Wing Cap')  }" />
             <img src="/img/sm64/blocks/block_blue.png" :class="{ 'opacity-25': !getNumberItemsFromName('Vanish Cap')  }" />
             <img src="/img/sm64/blocks/block_green.png" :class="{ 'opacity-25': !getNumberItemsFromName('Metal Cap')  }" />
 
-            <img src="/img/sm64/moves/climb.png" :class="{ 'opacity-25': !getNumberItemsFromName('Climb')  }" />
-            <img src="/img/sm64/moves/ledge_grab.png" :class="{ 'opacity-25': !getNumberItemsFromName('Ledge Grab')  }" />
-            <img src="/img/sm64/moves/ground_pound.png" :class="{ 'opacity-25': !getNumberItemsFromName('Ground Pound')  }" />
-            <img src="/img/sm64/moves/triple_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Triple Jump')  }" />
-            <img src="/img/sm64/moves/long_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Long Jump')  }" />
-            <img src="/img/sm64/moves/wall_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Wall Kick')  }" />
-            <img src="/img/sm64/moves/sideflip.png" :class="{ 'opacity-25': !getNumberItemsFromName('Side Flip')  }" />
-            <img src="/img/sm64/moves/backflip.png" :class="{ 'opacity-25': !getNumberItemsFromName('Backflip')  }" />
-            <img src="/img/sm64/moves/kick.png" :class="{ 'opacity-25': !getNumberItemsFromName('Kick')  }" />
-            <img src="/img/sm64/moves/dive.png" :class="{ 'opacity-25': !getNumberItemsFromName('Dive')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/climb.png" :class="{ 'opacity-25': !getNumberItemsFromName('Climb')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/ledge_grab.png" :class="{ 'opacity-25': !getNumberItemsFromName('Ledge Grab')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/ground_pound.png" :class="{ 'opacity-25': !getNumberItemsFromName('Ground Pound')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/triple_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Triple Jump')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/long_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Long Jump')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/wall_jump.png" :class="{ 'opacity-25': !getNumberItemsFromName('Wall Kick')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/sideflip.png" :class="{ 'opacity-25': !getNumberItemsFromName('Side Flip')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/backflip.png" :class="{ 'opacity-25': !getNumberItemsFromName('Backflip')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/kick.png" :class="{ 'opacity-25': !getNumberItemsFromName('Kick')  }" />
+            <img v-if="moveShuffled()" src="/img/sm64/moves/dive.png" :class="{ 'opacity-25': !getNumberItemsFromName('Dive')  }" />
         </div>
     </div>
 </template>
