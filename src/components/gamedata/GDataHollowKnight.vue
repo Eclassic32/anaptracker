4ad5de3d99b388f124be1b9806814eea9ee033e1
Thomas GUERY <originaltompouce@gmail.com>
2025-12-31 12:40:31 +0100

    revamped Hollow Knight itemset

--- src/components/gamedata/GDataHollowKnight.vue
+++ src/components/gamedata/GDataHollowKnight.vue
@@ -4,23 +4,51 @@
     <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
         <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Goal</div>
 
+        <span v-if="grubHunt()" class="mr-2 text-xs"><span class="font-bold" :class="{ 'opacity-25': !getNumberItemsFromName('Grubl')  }"><img src="/img/hollow_knight/grub.png" />x{{ getNumberItemsFromName('Grub') }} </span> / {{ grubHunt() }}</span>
         <img src="/img/hollow_knight/Monomon.png" :class="{ 'opacity-25': !getNumberItemsFromName('Monomon')  }" />
         <img src="/img/hollow_knight/Lurien.png" :class="{ 'opacity-25': !getNumberItemsFromName('Lurien')  }" />
         <img src="/img/hollow_knight/Herrah.png" :class="{ 'opacity-25': !getNumberItemsFromName('Herrah')  }" />
-        <span class="mr-2"></span>
-        <img src="/img/hollow_knight/VoidHeart.png" :class="{ 'opacity-25': !getNumberItemsFromName('Void_Heart')  }" />
-        <img src="/img/hollow_knight/DreamGate.png" :class="{ 'opacity-25': !getNumberItemsFromName('Dream_Gate')  }" />
+        <span v-if="radianceGoal()" class="mr-2"></span>
+        <img v-if="siblingsGoal() && getNumberItemsFromName('King_Fragment') && getNumberItemsFromName('Queen_Fragment')" src="/img/hollow_knight/Kingsoul.png" />
+        <img v-else-if="siblingsGoal() && getNumberItemsFromName('King_Fragment')" src="/img/hollow_knight/Charm_KingSoul_Left.png" />
+        <img v-else-if="siblingsGoal() && getNumberItemsFromName('Queen_Fragment')" src="/img/hollow_knight/Charm_KingSoul_Right.png" />
+        <img v-else-if="siblingsGoal()" class="opacity-25" src="/img/hollow_knight/Kingsoul.png" />
+        <img v-if="radianceGoal()" src="/img/hollow_knight/VoidHeart.png" :class="{ 'opacity-25': !getNumberItemsFromName('Void_Heart')  }" />
     </div>
     <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
         <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Moveset</div>
 
         <img v-if="!getNumberItemsFromName('Awoken_Deam_Nail')" src="/img/hollow_knight/DreamNail1.png" :class="{ 'opacity-25': !getNumberItemsFromName('Dream_Nail')  }" />
         <img v-else src="/img/hollow_knight/DreamNail2.png" />
-        <img v-if="!getNumberItemsFromName('Shade_Cloak')" src="/img/hollow_knight/MothwingCloak.png" :class="{ 'opacity-25': !getNumberItemsFromName('Mothwing_Cloak')  }" />
-        <img v-else src="/img/hollow_knight/ShadeCloak.png" />
-        <!-- Left_Wing_Mowthing_Cloak and Left_Wing_Mowthing_Cloak -->
-        <img src="/img/hollow_knight/MantisClaw.png" :class="{ 'opacity-25': !getNumberItemsFromName('Mantis_Claw')  }" />
-        <img src="/img/hollow_knight/CrystalHeart.png" :class="{ 'opacity-25': !getNumberItemsFromName('Crystal_Heart')  }" />
+        <img src="/img/hollow_knight/DreamGate.png" :class="{ 'opacity-25': !getNumberItemsFromName('Dream_Gate')  }" />
+
+
+        <!-- Left_Wing_Mowthing_Cloak and Right_Wing_Mowthing_Cloak -->
+        <span v-if="splitCloak()">
+            <img v-if="!getNumberItemsFromName('Left_Wing_Mowthing_Cloak')" src="/img/hollow_knight/MothwingCloak.png" class="opacity-25" />
+            <img v-else-if="!getNumberItemsFromName('Shade_Cloak')" src="/img/hollow_knight/MothwingCloak.png" />
+            <img v-else src="/img/hollow_knight/ShadeCloak.png" />
+            <img v-if="!getNumberItemsFromName('Right_Wing_Mowthing_Cloak')" src="/img/hollow_knight/MothwingCloak2.png" class="opacity-25" />
+            <img v-else-if="!getNumberItemsFromName('Shade_Cloak')" src="/img/hollow_knight/MothwingCloak2.png" />
+            <img v-else src="/img/hollow_knight/ShadeCloak2.png" />
+        </span>
+        <span v-else>
+            <img v-if="!getNumberItemsFromName('Shade_Cloak')" src="/img/hollow_knight/MothwingCloak.png" :class="{ 'opacity-25': !getNumberItemsFromName('Mothwing_Cloak')  }" />
+            <img v-else src="/img/hollow_knight/ShadeCloak.png" />
+        </span>
+
+        <span v-if="splitClaws()">
+            <img src="/img/hollow_knight/MantisClaw2.png" :class="{ 'opacity-25': !getNumberItemsFromName('Left_Mantis_Claw')  }" />
+            <img src="/img/hollow_knight/MantisClaw.png" :class="{ 'opacity-25': !getNumberItemsFromName('Right_Mantis_Claw')  }" />
+
+        </span>
+        <img v-else src="/img/hollow_knight/MantisClaw.png" :class="{ 'opacity-25': !getNumberItemsFromName('Mantis_Claw')  }" />
+        <span v-if="splitCrystal()">
+            <img src="/img/hollow_knight/CrystalHeart.png" :class="{ 'opacity-25': !getNumberItemsFromName('Left_Crystal_Heart')  }" />
+            <img src="/img/hollow_knight/CrystalHeart2.png" :class="{ 'opacity-25': !getNumberItemsFromName('Right_Crystal_Heart')  }" />
+
+        </span>
+        <img v-else src="/img/hollow_knight/CrystalHeart.png" :class="{ 'opacity-25': !getNumberItemsFromName('Crystal_Heart')  }" />
         <img src="/img/hollow_knight/IsmaTear.png" :class="{ 'opacity-25': !getNumberItemsFromName('Isma\'s_Tear')  }" />
         <img src="/img/hollow_knight/MonarchWings.png" :class="{ 'opacity-25': !getNumberItemsFromName('Monarch_Wings')  }" />
         <span class="mr-2"></span>
@@ -43,9 +71,13 @@
     *
     * Goal is to beat a set Final Boss
     * It can be :
-    *   - Hollow Knight : get 3 Dreamers
-    *   - Radiance :
-    *   - Siblings : 
+    *   - 0 - Any : Get any ending
+    *   - 1 - Hollow Knight : get 3 Dreamers
+    *   - 2 - Siblings : get 3 Dreamers plus both White fragments and Void Heart
+    *   - 2 - Radiance : get 3 Dreamers, Dream Nail and Void Heart
+    *   - 3 - Godhome : get 3 Dreamers, Dream Nail and Void Heart
+    *   - 4 - Godhome flower : get 3 Dreamers, Dream Nail and Void Heart
+    *   - 5 - Grub Hunt : get Grubs
     *
     */
 export default {
@@ -65,6 +97,42 @@ export default {
   },
 
         methods: {
+            grubHunt: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && this.data.slot_data.options.Goal == 5) {
+                    return this.data.slot_data.options.GrubHuntGoal;
+                }
+                return 0;
+            },
+            siblingsGoal: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && this.data.slot_data.options.Goal != 2) {
+                    return 0;
+                }
+                return 1;
+            },
+            radianceGoal: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && ![2, 3, 4, 5].includes(this.data.slot_data.options.Goal)) {
+                    return 0;
+                }
+                return 1;
+            },
+            splitClaws: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && ![2, 3, 4, 5].includes(this.data.slot_data.options.SplitMantisClaw)) {
+                    return 0;
+                }
+                return 1;
+            },
+            splitCloak: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && ![2, 3, 4, 5].includes(this.data.slot_data.options.SplitMothwingCloak)) {
+                    return 0;
+                }
+                return 1;
+            },
+            splitCrystal: function () {
+                if (this.data.slot_data.hasOwnProperty('options') && ![2, 3, 4, 5].includes(this.data.slot_data.options.SplitCrystalHeart)) {
+                    return 0;
+                }
+                return 1;
+            },
             getImageClass: function () {
                 if (this.$parent.$parent.$parent.OPTIONS.row_size) {
                     return 'iconbar-L my-1';
