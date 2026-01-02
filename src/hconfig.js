eccdaeb5996cdb6abc8dd083d00940bc1a745338
Thomas GUERY <originaltompouce@gmail.com>
2026-01-02 12:17:28 +0100

    fix : Production values.

--- src/hconfig.js
+++ src/hconfig.js
@@ -1,7 +1,7 @@
 var H_CONFIG = {
     BASE_DIRECTORY: '/anaptracker/',
-    URL_WEBSITE: 'http://localhost:5173',
-    //URL_WEBSITE: 'https://tomagueri.fr/anaptracker',
+    //URL_WEBSITE: 'http://localhost:5173',
+    URL_WEBSITE: 'https://tomagueri.fr/anaptracker',
 };
 
 export default H_CONFIG;
