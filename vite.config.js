5df9472ff826100a3d4c4ba015c58dbd0803d440
Thomas GUERY <originaltompouce@gmail.com>
2026-01-12 13:09:54 +0100

    production values

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  /*base: '/anaptracker',*/
+  base: '/anaptracker',
   plugins: [
     vue(),
         tailwindcss(),
