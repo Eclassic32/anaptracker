0260c9e2e7f186a7a5bbf6e73c1021c85851291e
Thomas GUERY <originaltompouce@gmail.com>
2026-01-13 08:56:00 +0100

    v0.7.0

--- src/anapconfig.js
+++ src/anapconfig.js
@@ -1,6 +1,6 @@
 var ANAP_CONFIG = {
     BASE_DIRECTORY: '/anaptracker/',
-    APP_VERSION: '0.6.1',
+    APP_VERSION: '0.7.0',
     //URL_WEBSITE: 'http://localhost:5173',
     URL_WEBSITE: 'https://tomagueri.fr/anaptracker',
 };
