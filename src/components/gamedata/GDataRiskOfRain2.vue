b04f14cd5a7cadea1566f84ee9168789a5deec33
Thomas GUERY <originaltompouce@gmail.com>
2026-01-14 04:59:32 +0100

    Risk of Rain 2 : better stage display handling

--- src/components/gamedata/GDataRiskOfRain2.vue
+++ src/components/gamedata/GDataRiskOfRain2.vue
@@ -3,36 +3,36 @@
     <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
         <div v-if="$parent.get_size()" class="text-xs font-normal text-left">Stages</div>
 
-        <img title="Distant Roost" src="/img/risk_of_rain_2/distant_roost.png" :class="{ 'opacity-35': !getNumberItemsFromName('Distant Roost')  }" />
-        <img title="Titanic Plains" src="/img/risk_of_rain_2/titanic_plains.png" :class="{ 'opacity-35': !getNumberItemsFromName('Titanic Plains')  }" />
-        <img v-if="hasDLC1()" title="Siphoned Forest" src="/img/risk_of_rain_2/siphoned_forest.png" :class="{ 'opacity-35': !getNumberItemsFromName('Siphoned Forest')  }" />
-        <img v-if="hasDLC1()" title="Verdant Falls" src="/img/risk_of_rain_2/verdant_falls.png" :class="{ 'opacity-35': !getNumberItemsFromName('Verdant Falls')  }" />
+        <img title="Distant Roost" src="/img/risk_of_rain_2/distant_roost.png" :class="{ 'opacity-35': !stageUnlocked('Distant Roost')  }" />
+        <img title="Titanic Plains" src="/img/risk_of_rain_2/titanic_plains.png" :class="{ 'opacity-35': !stageUnlocked('Titanic Plains')  }" />
+        <img v-if="hasDLC1()" title="Siphoned Forest" src="/img/risk_of_rain_2/siphoned_forest.png" :class="{ 'opacity-35': !stageUnlocked('Siphoned Forest')  }" />
+        <img v-if="hasDLC1()" title="Verdant Falls" src="/img/risk_of_rain_2/verdant_falls.png" :class="{ 'opacity-35': !stageUnlocked('Verdant Falls')  }" />
         <span class="mr-2"></span>
-        <img title="Abandoned Aqueduct" src="/img/risk_of_rain_2/abandoned_aqueduct.png" :class="{ 'opacity-10': !getNumberItemsFromName('Abandoned Aqueduct') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Abandoned Aqueduct', 1) }" />
-        <img title="Wetland Aspect" src="/img/risk_of_rain_2/wetland_aspect.png" :class="{ 'opacity-10': !getNumberItemsFromName('Wetland Aspect') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Wetland Aspect', 1) }" />
-        <img v-if="hasDLC1()" title="Aphelian Sanctuary" src="/img/risk_of_rain_2/aphelian_sanctuary.png" :class="{ 'opacity-15': !getNumberItemsFromName('Aphelian Sanctuary') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Aphelian Sanctuary', 1)  }" />
+        <img title="Abandoned Aqueduct" src="/img/risk_of_rain_2/abandoned_aqueduct.png" :class="{ 'opacity-10': !stageUnlocked('Abandoned Aqueduct') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Abandoned Aqueduct', 1) }" />
+        <img title="Wetland Aspect" src="/img/risk_of_rain_2/wetland_aspect.png" :class="{ 'opacity-10': !stageUnlocked('Wetland Aspect') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Wetland Aspect', 1) }" />
+        <img v-if="hasDLC1()" title="Aphelian Sanctuary" src="/img/risk_of_rain_2/aphelian_sanctuary.png" :class="{ 'opacity-15': !stageUnlocked('Aphelian Sanctuary') && !getNumberItemsFromName('Progressive Stage'), 'opacity-35': xOrStageLevel('Aphelian Sanctuary', 1)  }" />
         <span class="mr-2"></span>
-        <img title="Rallypoint Delta" src="/img/risk_of_rain_2/rallypoint_delta.png" :class="{ 'opacity-10': !getNumberItemsFromName('Rallypoint Delta') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Rallypoint Delta', 2) }" />
-        <img title="Scorched Acres" src="/img/risk_of_rain_2/scorched_acres.png" :class="{ 'opacity-10': !getNumberItemsFromName('Scorched Acres') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Scorched Acres', 2)  }" />
-        <img v-if="hasDLC1()" title="Sulfur Pools" src="/img/risk_of_rain_2/sulfur_pools.png" :class="{ 'opacity-10': !getNumberItemsFromName('Sulfur Pools') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Sulfur Pools', 2) }" />
+        <img title="Rallypoint Delta" src="/img/risk_of_rain_2/rallypoint_delta.png" :class="{ 'opacity-10': !stageUnlocked('Rallypoint Delta') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Rallypoint Delta', 2) }" />
+        <img title="Scorched Acres" src="/img/risk_of_rain_2/scorched_acres.png" :class="{ 'opacity-10': !stageUnlocked('Scorched Acres') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Scorched Acres', 2)  }" />
+        <img v-if="hasDLC1()" title="Sulfur Pools" src="/img/risk_of_rain_2/sulfur_pools.png" :class="{ 'opacity-10': !stageUnlocked('Sulfur Pools') && getNumberItemsFromName('Progressive Stage') < 2, 'opacity-35': xOrStageLevel('Sulfur Pools', 2) }" />
         <span class="mr-2"></span>
-        <img title="Abyssal Depths" src="/img/risk_of_rain_2/abyssal_depths.png" :class="{ 'opacity-10': !getNumberItemsFromName('Abyssal Depths') && getNumberItemsFromName('Progressive Stage') < 3,  'opacity-35': xOrStageLevel('Abyssal Depths', 3)  }" />
-        <img title="Siren's Call" src="/img/risk_of_rain_2/siren_call.png" :class="{ 'opacity-10': !getNumberItemsFromName('Siren\'s Call') && getNumberItemsFromName('Progressive Stage') < 3, 'opacity-35': xOrStageLevel('Siren\'s Call', 3)  }" />
-        <img title="Sundered Grove" src="/img/risk_of_rain_2/sundered_grove.png" :class="{ 'opacity-10': !getNumberItemsFromName('Sundered Grove') && getNumberItemsFromName('Progressive Stage') < 3, 'opacity-35': xOrStageLevel('Sundered Grove', 3) }" />
+        <img title="Abyssal Depths" src="/img/risk_of_rain_2/abyssal_depths.png" :class="{ 'opacity-10': !stageUnlocked('Abyssal Depths') && getNumberItemsFromName('Progressive Stage') < 3,  'opacity-35': xOrStageLevel('Abyssal Depths', 3)  }" />
+        <img title="Siren's Call" src="/img/risk_of_rain_2/siren_call.png" :class="{ 'opacity-10': !stageUnlocked('Siren\'s Call') && getNumberItemsFromName('Progressive Stage') < 3, 'opacity-35': xOrStageLevel('Siren\'s Call', 3)  }" />
+        <img title="Sundered Grove" src="/img/risk_of_rain_2/sundered_grove.png" :class="{ 'opacity-10': !stageUnlocked('Sundered Grove') && getNumberItemsFromName('Progressive Stage') < 3, 'opacity-35': xOrStageLevel('Sundered Grove', 3) }" />
         <span class="mr-2"></span>
-        <img title="Sky Meadow" src="/img/risk_of_rain_2/sky_meadow.png" :class="{ 'opacity-10': !getNumberItemsFromName('Sky Meadow') && getNumberItemsFromName('Progressive Stage') < 4,  'opacity-35': xOrStageLevel('Sky Meadow', 4) }" />
+        <img title="Sky Meadow" src="/img/risk_of_rain_2/sky_meadow.png" :class="{ 'opacity-10': !stageUnlocked('Sky Meadow') && getNumberItemsFromName('Progressive Stage') < 4,  'opacity-35': xOrStageLevel('Sky Meadow', 4) }" />
         <span class="mr-2"></span>
-        <img title="Commencement" src="/img/risk_of_rain_2/commencement.png" :class="{ 'opacity-10': !getNumberItemsFromName('Commencement') && getNumberItemsFromName('Progressive Stage') < 5, 'opacity-35': xOrStageLevel('Commencement', 4)  }" />
+        <img title="Commencement" src="/img/risk_of_rain_2/commencement.png" :class="{ 'opacity-10': !stageUnlocked('Commencement') && getNumberItemsFromName('Progressive Stage') < 5, 'opacity-35': xOrStageLevel('Commencement', 4)  }" />
 
         <span v-if="limboRoute()" class="mr-2"></span>
-        <img v-if="limboRoute()" title="Hidden Realm: A Moment, Fractured" src="/img/risk_of_rain_2/a_moment_fractured.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: A Moment, Fractured')  }" />
-        <img v-if="limboRoute()" title="Hidden Realm: A Moment, Whole" src="/img/risk_of_rain_2/a_moment_whole.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: A Moment, Whole')   }" />
+        <img v-if="limboRoute()" title="Hidden Realm: A Moment, Fractured" src="/img/risk_of_rain_2/a_moment_fractured.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: A Moment, Fractured')  }" />
+        <img v-if="limboRoute()" title="Hidden Realm: A Moment, Whole" src="/img/risk_of_rain_2/a_moment_whole.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: A Moment, Whole')   }" />
         
         <span v-if="voidlingRoute()" class="mr-2"></span>
-        <img v-if="voidlingRoute()" title="Hidden Realm: Bazaar Between Time" src="/img/risk_of_rain_2/bazaar_between_time.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: Bazaar Between Time')   }" />
-        <img v-if="voidlingRoute()" title="Hidden Realm: Void Fields" src="/img/risk_of_rain_2/void_fields.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: Void Fields')   }" />
-        <img v-if="voidlingRoute()" title="Hidden Realm: Void Locus" src="/img/risk_of_rain_2/void_locus.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: Void Locus')   }" />
-        <img v-if="voidlingRoute()" title="Hidden Realm: The Planetarium" src="/img/risk_of_rain_2/the_planetarium.png" :class="{ 'opacity-35': !getNumberItemsFromName('Hidden Realm: The Planetarium')   }" />
+        <img v-if="voidlingRoute()" title="Hidden Realm: Bazaar Between Time" src="/img/risk_of_rain_2/bazaar_between_time.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: Bazaar Between Time')   }" />
+        <img v-if="voidlingRoute()" title="Hidden Realm: Void Fields" src="/img/risk_of_rain_2/void_fields.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: Void Fields')   }" />
+        <img v-if="voidlingRoute()" title="Hidden Realm: Void Locus" src="/img/risk_of_rain_2/void_locus.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: Void Locus')   }" />
+        <img v-if="voidlingRoute()" title="Hidden Realm: The Planetarium" src="/img/risk_of_rain_2/the_planetarium.png" :class="{ 'opacity-35': !stageUnlocked('Hidden Realm: The Planetarium')   }" />
     </div>
 </template>
     
@@ -76,12 +76,23 @@ export default {
                 return true;
             },
             xOrStageLevel: function (stage, level) {
-                if ((this.getNumberItemsFromName('Progressive Stage') < level || !this.getNumberItemsFromName('Stage' + level.toString())) && this.getNumberItemsFromName(stage) ||
-                    (this.getNumberItemsFromName('Progressive Stage') >= level || this.getNumberItemsFromName('Stage' + level.toString())) && !this.getNumberItemsFromName(stage)) {
+                if ((this.getNumberItemsFromName('Progressive Stage') < level && !this.getNumberItemsFromName('Stage' + level.toString())) && this.stageUnlocked(stage) ||
+                    (this.getNumberItemsFromName('Progressive Stage') >= level || this.getNumberItemsFromName('Stage' + level.toString())) && !this.stageUnlocked(stage)) {
                     return true;
                 }
                 return false;
+            },
+            stageUnlocked: function (stage) {
+                if (this.getNumberItemsFromName(stage) || !this.requireStages())
+                    return true;
+                return false;
             }, 
+            requireStages: function () {
+                if (this.data.slot_data.hasOwnProperty('requireStages')) {
+                    return this.data.slot_data.requireStages;
+                }
+                return 1;
+            },
             getVictory: function () {
                 if (this.data.slot_data.hasOwnProperty('goal')) {
                     return this.data.slot_data.goal;
