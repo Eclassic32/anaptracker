f2f9277df7b19efbdad4cb4e23dc6b832e709392
Thomas GUERY <originaltompouce@gmail.com>
2026-01-14 13:05:36 +0100

    Secret of Evermore : fix Thunderball

--- src/components/gamedata/GDataSecretOfEvermore.vue
+++ src/components/gamedata/GDataSecretOfEvermore.vue
@@ -36,7 +36,7 @@
 
             <img v-if="getNumberItemsFromName('30 Cryo Blast')" title="Cryo Blast" src="/img/secret_of_evermore/weapons/ammo_3.png" />
             <img v-else-if="getNumberItemsFromName('30 Particle Bombs')" title="Particle Bomb" src="/img/secret_of_evermore/weapons/ammo_2.png" />
-            <img v-else-if="!getNumberItemsFromName('30 Thunderball')" " title="Thunderball" src="/img/secret_of_evermore/weapons/ammo_1.png" />
+            <img v-else-if="getNumberItemsFromName('30 Thunderball')" title="Thunderball" src="/img/secret_of_evermore/weapons/ammo_1.png" />
             <img v-else title="Bazooka" src="/img/secret_of_evermore/weapons/bazooka.png" class="opacity-25" :class="{ 'opacity-25': !getNumberItemsFromName('Bazooka')  }"/>
 
         </div>
